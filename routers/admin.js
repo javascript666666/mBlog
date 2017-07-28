e97c9eb8322cc4a9730e4682c0033eb432a25355
hanjianwen <javascript666666@163.com>
2017-07-28 09:59:10 +0800

    add User

--- routers/admin.js
+++ routers/admin.js
@@ -26,24 +26,56 @@ router.get('/', function(req, res) {
 * */
 
 router.get('/user',function(req, res){
-    //从数据库中读取所有用户数据
+    /*
+    * 从数据库中读取所有用户数据
+    *
+    * 数据分页显示功能
+    *     * query.limit(10);// 最多返回 10 条结果
+    * query.skip(10);// 跳过 10 条结果
+    * query.count() 统计中数
+    *
+    * 每页显示5 条
+    * page1:  1-5  skip:0 ->(page-1)*limit
+    * page2:  6-10  skip:1
+     */
+
+    var page = Number(req.query.page || 1);
+    var limit = 10;
+    var pages = 0;
+
     var query = new AV.Query('User');
-    query.descending('createdAt');
-    query.find().then(function (result) {
-        var users = result.map(function(item, index){
-            return {
-                _id: item.id,
-                username:item.attributes.username,
-                createdAt:item.createdAt.toString(),
-                isAdmin: Boolean(item.attributes.isAdmin)
-            }
-        });
-        //console.log( users);
-        res.render('admin/user_index', {
-            userInfo: req.userInfo,
-            users: users
-        });
-    })
+    //查询总数:
+    query.count().then(function (count) {
+       pages = Math.ceil( count/limit);
+       page = Math.min(page, pages);
+       page = Math.max(page,1);
+       var skip = (page -1) * limit;
+       console.log(pages);
+
+       query.descending('createdAt');
+       query.limit(limit);
+       query.skip(skip);
+       query.find().then(function (result) {
+           var users = result.map(function(item, index){
+               return {
+                    _id: item.id,
+                    username:item.attributes.username,
+                    createdAt:item.createdAt.toString(),
+                    isAdmin: Boolean(item.attributes.isAdmin)
+               }
+           });
+           //console.log( users);
+           res.render('admin/user_index', {
+               userInfo: req.userInfo,
+               users: users,
+
+               page: page,
+               limit: limit,
+               pages: pages,
+               count: count
+            });
+        })
+    }, function (error) {});
 })
 
 /*
